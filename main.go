c505fbbcf42b8a331bc2b24cdc28a2558ac244e8
Troels Thomsen <troels@thomsen.io>
2013-09-06 21:41:22 -0700

    Return environment variable HKAPP if set

--- main.go
+++ main.go
@@ -170,6 +170,10 @@ func getCreds(u *url.URL) (user, pass string) {
 }
 
 func app() (string, error) {
+	if app := os.Getenv("HKAPP"); app != "" {
+		return app, nil
+	}
+
 	var mustBeGitRemote bool
 	if flagApp == "" {
 		flagApp = "heroku"
